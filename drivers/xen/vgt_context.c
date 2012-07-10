2757aaefbc067a437da994d3faa01feff83b1851
Dexuan Cui <dexuan.cui@intel.com>
2012-07-10 16:36:28 +0800

    Add a temporary dom0 kernel option: small_per_vm_resource
    The previous commit
    "Made some changes to allow the creation of VM2"
    blocks Win8 guest because Win8 requires at least 128MB aperture size.
    
    So let's restore to the old behavior by default, and if we want try
    2 Linux VM guests now, we can enable this option.
    
    Dexuan is implementing a more flexible solution that allows us to specify
    the guest config parameters in HVM config file.
    
    Signed-off-by: Dexuan Cui <dexuan.cui@intel.com>

--- drivers/xen/vgt_context.c
+++ drivers/xen/vgt_context.c
@@ -158,6 +158,22 @@ static int __init vgt_primary_setup(char *str)
 }
 __setup("vgt_primary", vgt_primary_setup);
 
+// By default this temporary option is off, meaning a big per-vm resource
+// config, i.e.,  VGT_MIN_APERTURE_SZ=128MB and vgt_visible_fence_sz=8.
+// By passing "small_per_vm_resource" to the dom0 kernel, we use a smaller
+// config: i.e., VGT_MIN_APERTURE_SZ=64MB and vgt_visible_fence_sz=4.
+// To create 2 Linux HVM guests, we have to enable this option.
+//
+// Dexuan is implementing a more flexible solution that allows us to specify
+// the guest config parameters in HVM config file.
+static bool small_per_vm_resource = false;
+static int __init small_per_vm_resource_setup(char *str)
+{
+	small_per_vm_resource = true;
+	return 1;
+}
+__setup("small_per_vm_resource", small_per_vm_resource_setup);
+
 static struct pgt_device default_device = {
 	.bus = 0,
 	.devfn = 0x10,		/* BDF: 0:2:0 */
