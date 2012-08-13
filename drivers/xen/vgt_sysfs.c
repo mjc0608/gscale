a40901767e494ae4966aa598dd46579d7a620701
Anhua Xu <anhua.xu@intel.com>
2012-08-13 15:43:27 +0800

    sysfs: change file name 'hot_plug_event' to 'virtual_event'
    The hot_plug_event file under /sys/kernel/vgt/control actually
    was used to trigger virtual irq, so after talked with Eddie,
    this node renamed as 'virtual_event'. And this is the name
    Eddie gonna to present to Citrix.
    
    by axu

--- drivers/xen/vgt_sysfs.c
+++ drivers/xen/vgt_sysfs.c
@@ -301,7 +301,7 @@ static struct kobj_attribute display_pointer_attrs =
 	__ATTR(display_pointer, 0660, vgt_display_pointer_show, vgt_display_pointer_store);
 
 static struct kobj_attribute hot_plug_event_attrs =
-	__ATTR(hot_plug_event, 0660, vgt_hot_plug_reader, vgt_hot_plug_trigger);
+	__ATTR(virtual_event, 0660, vgt_hot_plug_reader, vgt_hot_plug_trigger);
 
 static struct kobj_attribute reg_owner_attrs =
 	__ATTR(reg_owner, 0660, vgt_reg_owner_show, vgt_reg_owner_store);
