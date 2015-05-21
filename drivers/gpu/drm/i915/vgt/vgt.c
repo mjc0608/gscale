a47a6517243a20f32e8b0854cca082fbe9fba719
Zhiyuan Lv <zhiyuan.lv@intel.com>
2015-05-21 14:38:07 +0800

    Remove the forcewake workaround
    It is not needed after we adding 0xfdc handling in force wake operations.
    
    Signed-off-by: Zhiyuan Lv <zhiyuan.lv@intel.com>

--- drivers/gpu/drm/i915/vgt/vgt.c
+++ drivers/gpu/drm/i915/vgt/vgt.c
@@ -796,15 +796,6 @@ static int vgt_initialize(struct pci_dev *dev)
 
 	printk("vgt_initialize succeeds.\n");
 
-	/* FIXME
-	 * always enable forcewake. It was found that forcewake
-	 * operation is one of the stability issue for running
-	 * windows guest. Before having a decent fix, we will
-	 * always enable force wake for Broadwell.
-	 */
-	if (IS_BDW(pdev))
-		vgt_force_wake_get();
-
 	return 0;
 err:
 	printk("vgt_initialize failed.\n");
